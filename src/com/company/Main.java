9e27e0fad89e2f1547b5d8bdf95de74b7c1eb259
79alexstud <79alexstud@gmail.com>
2022-04-17 17:31:59 +0300

    Лаб2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,33 @@
 package com.company;
+//Game - Lets,s guess number
+import java.util.Scanner;
+import java.util.Random;
+public class  Main {
 
-public static void main(String[] args) {
-        Car bugatti = new Car("Bugatti Veyron", 407);
-        System.out.println(bugatti.model);
-        System.out.println(bugatti.maxSpeed);
-    }
+public static void main (String[] args) {
+        int compNumber = random();
+        for (int i = 0; i < 3; i++) {
+                int humNumber = scan();
+                if  (humNumber == compNumber) {
+                        System.out.println ("Ты выиграл(а)");
+                        i = 3;
+                } else {
+                       if (i != 2) {System.out.println("Попробуй еще раз, осталось попыток в кол-ве " + (2-i));}
+                else {System.out.println("Ты проиграл(а)");}
+                }
+                }
+        }
 
-}
+public static int random() {
+        Random newRandom = new Random();
+        int randoNumber = newRandom.nextInt(11) + 1;
+        return randoNumber;
+        }
+
+public static int scan(){
+        System.out.println("Угадай число от 1 до 10");
+        Scanner scanner = new Scanner(System.in);
+        int humanNumber = scanner.nextInt();
+        return humanNumber;
+        }
+        }
