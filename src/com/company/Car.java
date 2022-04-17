f452e392a6393ca7a752c0293ed1c38fde4247a5
79alexstud <79alexstud@gmail.com>
2022-04-17 14:48:28 +0300

    Two labs

---
+++ src/com/company/Car.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Car {
+    String model;
+    int maxSpeed;
+
+    public Car(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
+    }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,9 @@
 package com.company;
-import java.util.Scanner;
 
-public class Main {
-    public static void main (String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите первое число: ");
-        int a = scan.nextInt();
-        System.out.print("Введите второе число: ");
-        int b = scan.nextInt();
-        System.out.print("Введите третье число: ");
-        int c = scan.nextInt();
-        System.out.println("Сумма чисел = " + (a + b + c));
+public static void main(String[] args) {
+        Car bugatti = new Car("Bugatti Veyron", 407);
+        System.out.println(bugatti.model);
+        System.out.println(bugatti.maxSpeed);
     }
+
 }
