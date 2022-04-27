83998c30227bf4ed633e954b18124ce5699c1d44
79alexstud <79alexstud@gmail.com>
2022-04-27 21:48:01 +0300

    Лаб 7 Интерфейсы доработка main + имплементация в reader1 интерфейса администратора

--- src/Lab7/Main.java
+++ src/Lab7/Main.java
@@ -6,12 +6,15 @@ public class Main {
         Reader1 read = new Reader1("Petrov Petr Petrovich","Petrov", "1234567");
         Provider1 prov = new Provider1("Sidorov Ivan Olegovich","Sidorov", "1234567");
         Administrator1 adm = new Administrator1("Ryzhikov Mefodiy Valerianovich","Ryzhikov", "1234567");
+        Reader1 usrdef = new Reader1("Petrov Petr Petrovich","Petrov", "1234567");
         read.takebook();
         adm.givebook();
         adm.findbook();
         adm.remind();
         libri1.orderbook();
         prov.getbook();
+        usrdef.takebook(); // реализация интерфейса читателя для роли читателя
+        usrdef.findbook(); // реализация интерфейса администратора для роли читателя
     }
 }
 
--- src/Lab7/Reader1.java
+++ src/Lab7/Reader1.java
@@ -1,6 +1,6 @@
 package Lab7;
 
-public class Reader1 extends User implements Reader {
+public class Reader1 extends User implements Reader,Administrator {
 
     Reader1 (String fio, String login, String phone){
       super (fio, login, phone);
@@ -11,4 +11,7 @@ public class Reader1 extends User implements Reader {
     public void retrieve () {
         System.out.println(getFio()+" вернул книгу");
     }
+    public void findbook (){System.out.println(getFio()+" нашел книгу");}
+    public void givebook () {System.out.println(getFio()+" отдал книгу");} // отдать книгу
+    public void remind (){System.out.println(getFio()+" У вас просрочка");};    // напомнить о просрочке
 }
